3368d739a5f97d059f05c326e89b5e33e1f41fd5
kiseop <kiseop91@naver.com>
2022-04-09 22:21:05 +0900

    Point Light

--- Source/Lighting/Lighting/Renderer.swift
+++ Source/Lighting/Lighting/Renderer.swift
@@ -59,6 +59,15 @@ class Renderer: NSObject {
         return light
     }()
     
+    lazy var redLight: Light = {
+       var light = buildDefaultLight()
+        light.type = Pointlight
+        light.color = [1, 0, 0]
+        light.attenuation = float3(1,3,4)
+        light.position = [-0, 0.5, -0.5]
+        return light
+    }()
+    
     var lights: [Light] = []
     var fragmentUniforms = FragmentUniforms()
     
@@ -98,6 +107,7 @@ class Renderer: NSObject {
       
       lights.append(sunlight)
       lights.append(ambientLight)
+      lights.append(redLight)
       fragmentUniforms.lightCount = UInt32(lights.count)
       
       
@@ -154,7 +164,7 @@ extension Renderer: MTKViewDelegate {
       }
     }
       
-      debugLights(renderEncoder: renderEncoder, lightType: Sunlight)
+      debugLights(renderEncoder: renderEncoder, lightType: Pointlight)
     renderEncoder.endEncoding()
     guard let drawable = view.currentDrawable else {
       return
